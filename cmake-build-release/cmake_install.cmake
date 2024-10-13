1cdf653560dae12610bdf8056fec8fcb0adbdef4
wpj <wpj24@mails.tsinghua.edu.cn>
2024-10-13 19:03:20 +0800

    10/13/00 update

--- cmake-build-release/cmake_install.cmake
+++ cmake-build-release/cmake_install.cmake
@@ -1,8 +1,8 @@
-# Install script for directory: C:/Users/LorenzLorentz/seadrive_root/王鹏杰/My Libraries/CSEFCloud/Notes&Homework&Assignment/Programming/Assignment
+# Install script for directory: /Users/lorenzlorentz/Library/CloudStorage/SeaDrive-王鹏杰(cloud.tsinghua.edu.cn)/我的资料库/CSEFCloud/Notes&Homework&Assignment/Programming/Assignment
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
-  set(CMAKE_INSTALL_PREFIX "C:/Program Files (x86)/Assignment")
+  set(CMAKE_INSTALL_PREFIX "/usr/local")
 endif()
 string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
 
@@ -34,7 +34,7 @@ endif()
 
 # Set default install directory permissions.
 if(NOT DEFINED CMAKE_OBJDUMP)
-  set(CMAKE_OBJDUMP "C:/Users/LorenzLorentz/AppData/Local/Programs/CLion/bin/mingw/bin/objdump.exe")
+  set(CMAKE_OBJDUMP "/usr/bin/objdump")
 endif()
 
 if(CMAKE_INSTALL_COMPONENT)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/LorenzLorentz/seadrive_root/王鹏杰/My Libraries/CSEFCloud/Notes&Homework&Assignment/Programming/Assignment/cmake-build-release/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/Users/lorenzlorentz/Library/CloudStorage/SeaDrive-王鹏杰(cloud.tsinghua.edu.cn)/我的资料库/CSEFCloud/Notes&Homework&Assignment/Programming/Assignment/cmake-build-release/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
